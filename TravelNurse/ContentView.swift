2cdc1dfb2242056322d662928b3fd8017c5b2769
Kojo <kodeon6@gmail.com>
2025-12-06 01:06:18 +0000

    Initial Commit

---
+++ TravelNurse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelNurse
+//
+//  Created by Kojo Kwakye on 06/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelNurse/TravelNurseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelNurseApp.swift
+//  TravelNurse
+//
+//  Created by Kojo Kwakye on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelNurseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
